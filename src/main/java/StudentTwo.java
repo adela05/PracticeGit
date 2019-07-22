1c06ce74bbfd8beb90ed5360f1f7bfeeb98bdb48
unknown <nnpt28@gmail.com>
2019-07-22 09:47:34 -0500

    added student2 class

---
+++ src/main/java/StudentTwo.java
@@ -0,0 +1,31 @@
+public class StudentTwo {
+    private int studentId;
+    private String name;
+    private String subject;
+
+    public StudentTwo(){}
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
